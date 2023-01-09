33ee803f8b4fecc324c1c936dc83b013e5399306
Oleksandr <alexgri1995@ukr.net>
2023-01-09 19:10:40 +0200

    add test

--- untitled/src/test/java/test/WebTests.java
+++ untitled/src/test/java/test/WebTests.java
@@ -7,9 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import utils.Links;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class WebTests {
     @Test
-    public void navigateToRozetkaWebsiteAndSearchSomething() {
+    public void navigateToRozetkaWebsiteAndSearchIphone() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(Links.ROZETKA.getLink());
@@ -17,10 +19,19 @@ public class WebTests {
                 .xpath("/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/div/form/div/div/input"));
         searchField.clear();
         searchField.sendKeys("iphone");
+        Thread.sleep(2000);
 
         WebElement searchButton = driver.findElement(By
-                .xpath("/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/div/form/button"));
+                        .xpath("/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/div/form/button"));
         searchButton.click();
+        Thread.sleep(2000);
+        WebElement header = driver.findElement(By
+                .xpath("/html/body/app-root/div/div/rz-category/div/main/div[1]/div/h1"));
+
+        assertThat(header.getText())
+                .as("Wrong search key")
+                .isEqualTo("Мобільні телефони Apple");
+        Thread.sleep(2000);
         driver.quit();
     }
 }
